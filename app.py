34cbb9c88313e6d8ef528bf502e0e8d3184831a6
Ubuntu <ubuntu@ip-172-31-40-198.us-east-2.compute.internal>
2025-05-11 19:55:54 +0000

    Built basic routes

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
-from flask import Flask
+from flask import Flask, redirect, render_template, request, session, url_for
 import os
 
 app = Flask(__name__)
+app.secret_key = os.urandom(16)
 
+@app.before_request
+def initialize_session():
+    if 'squares' not in session:
+        session['squares'] = {f'{key}' : " " for key in range(1,10)}
 
 @app.route("/")
 def index():
-    return "Getting started with tic tac toe"
+    return render_template('home.html')
+    
+@app.route('/play')
+def play():
+    return render_template('play.html')
+    
+@app.route('/verify', methods=["POST"])
+def verify():
+    new_square = request.form['new_square']
+    session['squares'][new_square] = "X"
+    session.modified = True
+    return redirect(url_for('play'))
     
 if __name__ == "__main__":
     if os.environ.get('FLASK_ENVIRON') == 'production':
